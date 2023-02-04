e98e074249c451f295725afe16ed9d9822bead8b
jeffigy <evangelistajeffy25@gmail.com>
2023-02-04 10:00:33 +0800

    lazy loading

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
+import React from 'react'
 import Navbar from 'components/Navbar';
-import About from 'pages/About';
+// import About from 'pages/About';
 import Home from 'pages/Home';
 import './index.css'
 import { Routes, Route } from 'react-router-dom'
@@ -11,13 +12,14 @@ import NewProducts from 'pages/NewProducts';
 import { Users } from 'pages/Users';
 import { UserDetails } from 'pages/UserDetails';
 import { Admin } from 'pages/Admin';
+const LazyAbout = React.lazy(() => import('pages/About'))
 function App() {
   return (
     <>
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='about' element={<About />} />
+        <Route path='about' element={<LazyAbout />} />
         <Route path='order-summary' element={<OrderSummary />} />
         <Route path='products' element={<Products />} >
           <Route index element={<FeaturedProducts />} />
@@ -25,7 +27,9 @@ function App() {
           <Route path='new' element={<NewProducts />} />
         </Route>
         <Route path='users' element={<Users />} >
+          {/* dynamic route */}
           <Route path=':id' element={<UserDetails />} />
+          {/* dynamic route that has a specific route */}
           <Route path='admin' element={<Admin />} />
         </Route>
         <Route path='*' element={<PageNotFound />} />
